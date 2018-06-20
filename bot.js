740473d4a31750f53fcd854e5200aefcc4ce0390
hamo21 <40078281+hamo21@users.noreply.github.com>
2018-06-20 06:29:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,5 +10,141 @@ client.on('message', message => {
         message.reply('pong');
       }
 });
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+      }
+});
+
+
+client.on('message', message => {
+    if(message.content === 'الشعار'){
+        message.channel.send(':arrow_down:شعار دسكورد جنب اسمك الاصلي')
+    }
+});
+
+
+client.on('message', message => {
+    if(message.content === 'الشعار'){
+        message.channel.send('``! KD |``')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === 'الشعار'){
+        message.channel.send(':arrow_down:شعار العبه')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === 'الشعار'){
+        message.channel.send('``! ᴷ ᴰ多``')
+    }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'برب') {
+    msg.reply('** ٌــيَــتٌ يَ حٍــلُــۆ،  لُآ تٌــطًــۆلُ :disappointed:**');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'ترحيب') {
+    msg.reply('https://i.imgur.com/B5wWkpZ.png');
+  }
+});
+
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'باك') {
+    msg.reply('**وّلّلّّكّكّممّّ نۨــۏڕٺ ۛ ּيۧ مۭــڗ ۛ ּ، ۛ ּمۭــنۨ ۖ ڙمۭــٰا̍نۨ ̨؏ــڼۨــک ۗ :rose::wave:**');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === '.') {
+    msg.reply('**يلبى الى ينقط كمل يا حلو **');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === '..') {
+    msg.reply('**لسا بدك تنقط كمل خلينا نشوف نقطك**');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === '...') {
+    msg.reply('**ما اجمل نقطك كمل كمان  **');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === '....') {
+    msg.reply('**خلاص ما بكمل كمل لوحدك **');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === '.....') {
+    msg.reply('**انت حمار لسا  بتسمع كلامى :joy: **');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === '......') {
+    msg.reply('**خلاص يا ابن الحلال وقف **');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'السلام عليكم') {
+    msg.reply('**ۆعـلُـيَـگـم آلُـسـلُآم ۆرحٍـمةّ آلُلُہ ۆبْرگآتٌہ ، منْۆر. :rose: **');
+  }
+});
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Kingdom of  Death⚔`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+  
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
